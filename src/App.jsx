068e8696fe23353f963a5e2a7fa2ae8723ab7b81
mrunalbhonde <bhondemrunal0@gmail.com>
2024-08-25 15:50:40 +0530

    added ability to store history

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,8 @@ function Square({ value, onSquareClick }){
   )
 }
 
-function Board() {
+function Board({ xIsNext, squares, onPlay }) {
   
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
   function handleClick(i){
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -25,8 +22,7 @@ function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -59,7 +55,56 @@ function Board() {
   )
 }
 
-export default Board
+function Game(){
+
+  // an array with a single item, which is the current state of the board, which is empty at start
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
+  
+
+  function handlePlay(nextSquares){
+    
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+
+  }
+
+  function jumpTo(nextMove){
+
+    setCurrentMove(nextMove);
+
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
 
 function calculateWinner(squares) {
   const lines = [
